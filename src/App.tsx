02acd6d2f5cad02bc448beb7ef89e4d32f580e24
Wilux <61833703+WiLuX-Source@users.noreply.github.com>
2025-08-28 16:15:02 +0300

    Finish migration of first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,23 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 function App() {
-  const [count, setCount] = useState(0)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="bg-gray-500 ">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className='font-system bg-bg-dark h-dvh flex flex-col items-center justify-center text-center'>
+      <div className="max-w-[800px] flex flex-col gap-6 items-center justify-center">
+        <h1 className="font-mono font-bold text-6xl" id="main-title">
+          <span className="text-wong-sky-blue">new</span>
+          &nbsp;
+          <span className="text-wong-orange">Date</span>
+          <span className="text-wong-grey">(</span>
+          <span className="text-wong-bluish-green">"wtf"</span>
+          <span className="text-wong-grey">)</span>
+        </h1>
+        <p className="text-gray-300 text-3xl italic">How well do you know JavaScript's Date class?</p>
+        <p className="text-gray-300 font-mono italic rounded-lg bg-bg-light text-lg p-4 text-balance w-[75%]">
+          All questions verified using NodeJS 24.4.0 on
+          a MacBook Pro set to the BST timezone (UTC+1).
         </p>
+        <button className="bg-wong-orange text-bg-dark border-none rounded-lg font-bold cursor-pointer max-w-[250px] w-full p-4 text-xl">Start Quiz</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
